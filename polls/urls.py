88212895a90c03a09b347b24f340a1592d3648cb
SamC <samcsoft@gmail.com>
2022-08-11 12:29:59 +0100

    created testcase

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import index
+from . import views
 
+app_name = 'polls'
 
 urlpatterns = [
-    path('', index, name='index'),
+    # #  The normal without the class view or Generic view
+    # path('', views.index, name='index'),
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
